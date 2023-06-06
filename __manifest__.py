39b837e44a55cf9868a380c1953f0aa931a5d001
AhmedElkhodary <ahmed.alkhodary99@gmail.com>
2023-06-06 05:34:51 +0300

    first prepare

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Library Management",
+    'summary': """Manage library catalog""",
+    'author': "Khodary",
+    'license': "AGPL-3",
+    'website': "",
+    # 'category': 'Uncategorized',
+    'category': 'Services/Library',
+    'version': '0.1',
+    'depends': ['base'],
+    'application': True,
+    'data': [],
+}
