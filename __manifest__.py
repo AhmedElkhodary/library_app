11fe44738a3502154720e85b74cad8108cd18766
AhmedElkhodary <ahmed.alkhodary99@gmail.com>
2023-06-08 05:24:48 +0300

    add

--- __manifest__.py
+++ __manifest__.py
@@ -4,10 +4,16 @@
     'author': "Khodary",
     'license': "AGPL-3",
     'website': "",
-    # 'category': 'Uncategorized',
     'category': 'Services/Library',
     'version': '0.1',
     'depends': ['base'],
     'application': True,
-    'data': [],
+    'data': [
+        'security/library_security.xml',
+        "security/ir.model.access.csv",
+        'views/book_view.xml',
+        'views/library_menu.xml',
+        'views/book_list_template.xml',
+
+    ],
 }
